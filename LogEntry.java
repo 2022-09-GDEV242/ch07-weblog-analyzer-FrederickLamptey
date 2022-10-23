4f5e9101bccc5730477c0ba7ff6234b1e497efcd
FrederickLamptey <102755255+FrederickLamptey@users.noreply.github.com>
2022-10-23 15:33:12 -0400

    add getMonth and getDay to logEntry

--- LogEntry.java
+++ LogEntry.java
@@ -71,7 +71,25 @@ public class LogEntry implements Comparable<LogEntry>
     {
         return dataValues[HOUR];
     }
-
+    
+      /**
+     * Return the day.
+     * @return The day field from the log line.
+     */
+    public int getDay()
+    {
+        return dataValues[DAY];
+    }
+    
+      /**
+     * Return the month.
+     * @return The month field from the log line.
+     */
+    public int getMonth()
+    {
+        return dataValues[MONTH];
+    }
+    
     /**
      * Return the minute.
      * @return The minute field from the log line.
